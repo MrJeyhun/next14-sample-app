bd6b56043512042d81b70220552d6b3566b40bcb
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-11-25 00:23:05 +0300

    created layout & initial ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
-import Link from "next/link";
+import homeImg from "public/home.jpg";
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
-    <div>
-      <div>
-        <Link href="/performance">Performance</Link>
-        <Link href="/reliability">Reliability</Link>
-        <Link href="/scale">Scale</Link>
-      </div>
-      <div>Home Page</div>
-    </div>
+    <Hero
+      imgData={homeImg}
+      imgAlt="car factory"
+      title="Professional Cloud Hosting"
+    />
   );
 }
